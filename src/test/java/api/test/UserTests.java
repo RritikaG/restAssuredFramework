f8db1034078bd5273e33cadd5614db69a25d7ca8
RritikaG <guptaritika579@gmail.com>
2025-01-18 16:18:06 +0530

    added all basic endpoints and basic data

--- src/test/java/api/test/UserTests.java
+++ src/test/java/api/test/UserTests.java
@@ -6,6 +6,7 @@ import com.github.javafaker.Faker;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class UserTests {
     Faker faker;
@@ -23,12 +24,41 @@ public class UserTests {
         userPayload.setPhone(faker.phoneNumber().cellPhone());
 
     }
-
+@Test(priority = 1)
     public void testPostUser()
     {
         Response response = UserEndPoint.createUser(userPayload);
         response.then().log().all();
 
+        Assert.assertEquals(response.getStatusCode(),200);
+    }
+    @Test(priority = 2)
+    public void testGetUserbyName()
+    {
+        Response response=UserEndPoint.readUser(this.userPayload.getUsername());
+        response.then().log().all();
+        Assert.assertEquals(response.getStatusCode(),200);
+
+    }
+
+    @Test(priority = 3)
+    public void testUpdateUserByName()
+    {
+        userPayload.setLastname(faker.name().lastName());
+        userPayload.setEmail(faker.internet().safeEmailAddress());
+        Response response = UserEndPoint.updateUser(this.userPayload.getUsername(),userPayload);
+        response.then().log().body().statusCode(200);
+
+        //check data after update
+        Response responseafterupdate=UserEndPoint.readUser(this.userPayload.getUsername());
+        response.then().log().all();
+        Assert.assertEquals(responseafterupdate.getStatusCode(),200);
+
+    }
+    @Test(priority = 4)
+    public void testDeleteUserByName()
+    {
+        Response response=UserEndPoint.deleteUser(this.userPayload.getUsername());
         Assert.assertEquals(response.getStatusCode(),200);
     }
 }
